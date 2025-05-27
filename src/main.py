69fa5cfaeba1bdce70135ba1c10bf60b8b9c636e
DarriusChen <darriuschen0926@gmail.com>
2025-05-28 02:03:16 +0800

    feat: Enhance main.py with validation and logging features
    - Integrate Result class for managing validation outcomes.
    
    - Implement schema validation and update results accordingly.
    
    - Add logging for validation steps and outcomes to improve traceability.
    
    - Read result path from config.ini for dynamic file handling.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 import argparse
+import configparser
 
 from file_validator import PathValidator
+
+from logger import setup_logger, format_func_msg
+from schema_validator import validate_data
+from result import Result
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+result_path = config['result_path']['path']
+
+logger = setup_logger(name=__name__, file_path=f'../output/logs/{__name__}.log')
 
 parser = argparse.ArgumentParser(description='Paybility')
 
@@ -13,11 +24,43 @@ parser.add_argument('--tables', type=list, help='表種', default=['表4', '表7
 
 def main(args):
     
-    pv = PathValidator(args)
+    # 創建 Result object
+    result_obj = Result(file=args.filepath, result_path=result_path)
     
-    pv.show_info()
+    # Step 1: file check
+    pv = PathValidator(args) # TODO: 傳入 result_obj
+    info = pv.show_info()
+    file_type = info['File_name']["Table"]
+    # file_check_result = pv.show_result() # TODO: 用result.py 更新result
     
+    # 更新 File level 檢查結果
+    # if result_obj.update_result('file_check', file_check_result["result"]["file_check"]):
+    #     logger.info(format_func_msg(func='main', msg="文件檢查結果更新成功"))
+    # else:
+    #     logger.error(format_func_msg(func='main', msg="文件檢查結果更新失敗"))
+
+    # ------------------------------------------------------------ #
     
+    # Step 2: schema check
+    schema_result = validate_data(
+        file_type=file_type,
+        data_file=args.filepath,
+    )
+    
+    # 更新 Schema 檢查結果
+    if result_obj.update_result('schema_check', schema_result):
+        logger.info(format_func_msg(func='main', msg="Schema 檢查結果更新成功"))
+    else:
+        logger.error(format_func_msg(func='main', msg="Schema 檢查結果更新失敗"))
+
+    # ------------------------------------------------------------ #
+     
+    # Last Step: 保存最終結果
+    if result_obj.save_result():
+        logger.info(format_func_msg(func='main', msg="驗證結果已成功保存"))
+    else:
+        logger.error(format_func_msg(func='main', msg="驗證結果保存失敗"))
+
 if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
