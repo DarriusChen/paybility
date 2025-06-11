e19728fd82967257dbf658bdc7f42e866383f42b
DarriusChen <darriuschen0926@gmail.com>
2025-06-11 16:05:47 +0800

    feat: Enhance schema validation with entity code verification
    - Implemented entity code validation logic in the validate_schema function to ensure compliance with specified formats.
    - Added a new function, validate_entity_code, to check the validity of entity codes against a loaded mapping DataFrame.
    - Updated the test_schema.py to include the county parameter for validation testing.

--- src/schema_validator.py
+++ src/schema_validator.py
@@ -3,10 +3,17 @@ import configparser
 from pathlib import Path
 import logging
 from logger import setup_logger, format_func_msg
-from utils import LOG_PATH, TEMPLETE_PATH, get_dict_template
+from utils import LOG_PATH, TEMPLETE_PATH, get_dict_template, load_data, MAPPING_FILE
 
 
 logger = setup_logger(name=__name__, file_path=f'{LOG_PATH}/{__name__}.log')
+
+# TODO: 業者代碼資料庫
+entity_mapping_df = load_data(path=MAPPING_FILE,
+                        sheet_name="4-1",
+                        header_rows=[0],
+                        index_col=[0],
+                        logger=logger)
 
 def load_complex_schema(path: str | Path,
                         header_rows: list[int] = [2, 3],
@@ -39,21 +46,41 @@ def load_complex_schema(path: str | Path,
 
     return df.columns.tolist()
 
+def validate_entity_code(county: str, entity_code: str) -> bool:
+    """驗證業者代碼是否符合格式。
+    
+    Args:
+        entity_code: 業者代碼
+    Returns:
+        bool: 是否符合格式
+    """
+    try:
+        entity_names = entity_mapping_df.loc[(county)]["系統業者代號"].tolist()
+        if entity_code in entity_names:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger.error(format_func_msg(func='validate_entity_code', msg=f"讀取業者代碼時發生錯誤: {e}"))
+        return None
+
 
 def validate_schema(file_type: str,
                   data_file: str | Path,
+                  county: str,
                   logger: logging.Logger = logger) -> dict:
     """驗證資料是否符合模板，並回傳驗證結果。
     
     Args:
         file_type: 表種
         data_file: 資料檔案路徑
+        county: 縣市
         logger: 日誌記錄器
     Returns:
         dict: 驗證結果
     """
 
-    template_file = f"{TEMPLETE_PATH}/增辦第4期-{file_type}.xlsx"
+    template_file = f"{TEMPLETE_PATH}/增辦第4期-表{file_type[-1]}.xlsx"
 
     schema_result = get_dict_template("schema_check")
     
@@ -80,6 +107,9 @@ def validate_schema(file_type: str,
 
     validation_passed = True
     error_messages = []
+    schema_result['sub_status']['column_name']['status'] = True
+    schema_result['sub_status']['column_name']['info'] = f"{file_type}"
+    schema_result['sub_status']['column_name']['message'] = "✅ 欄位名稱及順序正確"
 
     # 驗證欄位數量是否符合模板
     template_columns_len = len(template_columns)
@@ -127,6 +157,28 @@ def validate_schema(file_type: str,
         schema_result['sub_status']['column_name']['message'] = "❌ 欄位名稱錯誤"
         validation_passed = False
 
+    # 驗證業者代碼是否符合格式
+    schema_result['sub_status']['entity_code']['status'] = False
+    df_entity_result = load_data(path=data_file,
+                                 header_rows=[1],
+                                 logger=logger
+                                 )
+    entity_info = df_entity_result.columns.tolist()
+    if "業者" in entity_info[0]:
+        entity_code = entity_info[2]
+        if not validate_entity_code(county=county, entity_code=entity_code):
+            schema_result['sub_status']['entity_code']['info'] = f"錯誤業者代碼: {entity_code}"
+            schema_result['sub_status']['entity_code']['message'] = "❌ 業者代碼錯誤，請確認業者代碼是否正確"
+            validation_passed = False
+        else:
+            schema_result['sub_status']['entity_code']['status'] = True
+            schema_result['sub_status']['entity_code']['info'] = entity_code
+            schema_result['sub_status']['entity_code']['message'] = "✅ 業者代碼正確"
+    else:
+        schema_result['sub_status']['entity_code']['info'] = entity_info[0]
+        schema_result['sub_status']['entity_code']['message'] = f"❌ 業者代碼欄位名稱錯誤: {entity_info[0]} - {entity_info[2]}"
+        validation_passed = False
+
     logger.info(
         format_func_msg(func='validate_data',
                         msg=f"驗證完成，結果: {'通過' if validation_passed else '失敗'}"))
@@ -135,14 +187,6 @@ def validate_schema(file_type: str,
     if validation_passed:
         schema_result['status']['status'] = True
         schema_result['status']['message'] = "✅ 欄位結構正確"
-        
-        schema_result['sub_status']['entity_code']['status'] = True
-        schema_result['sub_status']['entity_code']['info'] = "業者代碼"
-        schema_result['sub_status']['entity_code']['message'] = "✅ 業者代碼正確"
-        
-        schema_result['sub_status']['column_name']['status'] = True
-        schema_result['sub_status']['column_name']['info'] = f"{file_type}: {data_columns_len}"
-        schema_result['sub_status']['column_name']['message'] = "✅ 欄位名稱及順序正確"
     else:
         schema_result['status']['info'] = f"錯誤訊息: {'; '.join(error_messages)}"
         schema_result['status']['message'] = "❌ 欄位結構錯誤"
