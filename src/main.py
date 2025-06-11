3be90aae896283ab085c8b638018605bda855f87
DarriusChen <darriuschen0926@gmail.com>
2025-06-11 16:28:51 +0800

    feat: Implement logic validation for matching numbers
    - Added a new module, logic_validator.py, to validate the format of matching numbers based on specified criteria.
    - Introduced the function is_valid_matching_number to perform detailed checks on matching codes.
    - Updated main.py to integrate the new validation step for matching numbers in the processing workflow.

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from file_validator import validate_path
 
 from logger import setup_logger, format_func_msg
 from schema_validator import validate_schema
+from logic_validator import validate_matching_numbers
 from result import Result
 # config
-from utils import RESULT_PATH, LOG_PATH
+from utils import RESULT_PATH, LOG_PATH, MAPPING_FILE
 
 logger = setup_logger(name=__name__, file_path=f'{LOG_PATH}/{__name__}.log')
 
@@ -21,8 +22,6 @@ def main(args):
     # Step 1: file check
     # pv = PathValidator(args) # TODO: 傳入 result_obj
     path_check_result, file_check_result, filename_check_result = validate_path(args.filepath)
-    file_type = filename_check_result["sub_status"]["table"]["info"]
-
     
     # 更新 File level 檢查結果
     # update path_check
@@ -46,13 +45,32 @@ def main(args):
     # ------------------------------------------------------------ #
     
     # Step 2: schema check
-    schema_result = validate_schema(file_type=file_type, data_file=args.filepath)
+    file_type = filename_check_result["sub_status"]["table"]["info"]
+    county = path_check_result["sub_status"]["county"]["info"]
+    schema_result = validate_schema(file_type=file_type,
+                                    data_file=args.filepath,
+                                    county=county)
     
     # 更新 Schema 檢查結果
     if result_obj.update_result('schema_check', schema_result):
         logger.info(format_func_msg(func='main', msg="Schema 檢查結果更新成功"))
     else:
         logger.error(format_func_msg(func='main', msg="Schema 檢查結果更新失敗"))
+
+    # ------------------------------------------------------------ #
+    # Step 3: logic check -1: 媒合編號
+    period = path_check_result["sub_status"]["period"]["info"]
+    logic_result_matching_numbers = validate_matching_numbers(
+                                            data_file=args.filepath,
+                                            logger=logger,
+                                            county=county,
+                                            phase=period)
+    if result_obj.update_result('logic_check_matching_numbers', logic_result_matching_numbers):
+        logger.info(format_func_msg(func='main', msg="媒合編號 檢查結果更新成功"))
+    else:
+        logger.error(format_func_msg(func='main', msg="媒合編號 檢查結果更新失敗"))
+
+    # Step 4: 
 
     # ------------------------------------------------------------ #
      
