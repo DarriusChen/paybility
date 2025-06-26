3473725214933f4c41a160483a7085f15619aebe
DarriusChen <darriuschen0926@gmail.com>
2025-06-27 02:07:02 +0800

    feat: Enhance Streamlit app with file uploader state management and restart functionality
    - Added a unique key for the file uploader to maintain state across sessions.
    - Implemented a restart function to clear logs and reset the uploader.
    - Improved button layout for validation and restart actions within the app.

--- src/test_streamlit.py
+++ src/test_streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import pandas as pd
 from utils.utils import RESULT_PATH
+import uuid
 
 # 用 session_state 儲存日誌，避免每次重新執行就消失
 if "log_lines" not in st.session_state:
@@ -9,6 +10,9 @@ if "log_lines" not in st.session_state:
 
 if 'show_result' not in st.session_state:
     st.session_state.show_result = False
+
+if "file_uploader" not in st.session_state:
+    st.session_state.file_uploader = str(uuid.uuid4())
 
 # Test function
 # TODO: 改成實際的驗證函數
@@ -24,19 +28,32 @@ def test_validate_file(files):
 def close_result():
     st.session_state.show_result = False
 
+def restart_app():
+    st.session_state.log_lines = []
+    st.session_state.show_result = False
+
+    # 清掉 uploader 的內容
+    st.session_state.file_uploader = str(uuid.uuid4())
+
 # TODO: 改成實際的結果 (result_obj)
 def streamlit_app(result_path: str = RESULT_PATH):
     st.title("📁 自動付款清冊系統")
     st.write("請上傳 Excel 檔案，系統將自動進行驗證")
-    files = st.file_uploader("上傳檔案", type=["xlsx", "xls"], accept_multiple_files=True, label_visibility="collapsed")
+    files = st.file_uploader("上傳檔案", type=["xlsx", "xls"], key=st.session_state.file_uploader, accept_multiple_files=True, label_visibility="collapsed")
+
+    left, middle, right = st.columns(3)
+
     if files:
         st.success("上傳成功! 共上傳 {} 個檔案".format(len(files)))
-        st.button("開始驗證", on_click=test_validate_file, args=(files,), key="validate_button")
+        with left:
+            st.button("開始驗證", on_click=test_validate_file, args=(files,), key="validate_button", icon="🔍",  use_container_width=True)
+        with right:
+            st.button("重新驗證", on_click=restart_app, key="restart_button", icon="🔄", use_container_width=True)
     else:
         st.warning("請上傳檔案")
     
     if st.session_state.show_result:
-        st.button("關閉結果", on_click=close_result, key="close_result_button")
+        st.button("關閉結果", on_click=close_result, key="close_result_button", icon="❌")
         with st.container(height=250, border=True):
             with open(result_path, "r") as f:
                 result = json.load(f)
